b5def1007f9e5e25eef6f57bceaa1c2eacede086
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-04-24 09:35:29 +0300

    updated task 2

--- js/task-02.js
+++ js/task-02.js
@@ -8,12 +8,24 @@ const ingredients = [
 ];
 
 const list = document.querySelector('ul');
-console.log(list)
+const elements = ingredients.map(ingredient => {
+    const liEl = document.createElement('li');
+    liEl.classList.add('item');
+    liEl.textContent = ingredient;
+    return liEl;
 
-ingredients.forEach(ingredient => {
-  const item = document.createElement('li');
-  item.classList.add('item');
-  item.textContent = ingredient;
-  list.appendChild(item)
-})
+  });
+
+list.append(...elements);
+
+console.log(list);
+
+
+// ingredients.forEach(ingredient => {
+//   const item = document.createElement('li');
+//   item.classList.add('item');
+//   item.textContent = ingredient;
+//   list.appendChild(item)
+// })
+
 
