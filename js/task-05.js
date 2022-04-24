1b393e0c4180692f1cea8c975bc8fcc54067f251
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-04-24 09:58:42 +0300

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,8 +2,10 @@ const inputName = document.querySelector('#name-input');
 const helloName = document.querySelector('#name-output');
 
 const inputNameTyping = (event) => {
-    if (event.currentTarget.value !== ' ') {
+    if (event.currentTarget.value !== '') {
         helloName.textContent = event.currentTarget.value;
+    } else {
+        helloName.textContent = 'Anonymous';
     }
 } 
 inputName.addEventListener('input', inputNameTyping)
